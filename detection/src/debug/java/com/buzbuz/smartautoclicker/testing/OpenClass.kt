dcc1bdd547869b4a774ef4bb99aabaa9ed8e799d
Buzeau Kevin <buzeau.kevin@gmail.com>
2020-11-14 11:32:24 +0100

    Integrate kotlin all open for tests

---
+++ detection/src/debug/java/com/buzbuz/smartautoclicker/testing/OpenClass.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 Nain57
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.buzbuz.smartautoclicker.testing
+
+/**
+ * This annotation allows us to open some classes for mocking purposes while they are final in
+ * release builds.
+ */
+@Target(AnnotationTarget.ANNOTATION_CLASS)
+annotation class OpenClass
+
+/**
+ * Annotate a class with [OpenForTesting] if you want it to be extendable in debug builds.
+ */
+@OpenClass
+@Target(AnnotationTarget.CLASS)
+annotation class OpenForTesting
