d37c170b4b76643fd748fb50cb2e686f8e5a30b5
Buzeau Kevin <buzeau.kevin@gmail.com>
2020-11-22 10:48:08 +0100

    Add missing OpenForTesting package in release

--- detection/src/release/java/com/buzbuz/smartautoclicker/testing/OpenClass.kt
+++ detection/src/release/java/com/buzbuz/smartautoclicker/testing/OpenClass.kt
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; If not, see <http://www.gnu.org/licenses/>.
  */
+package com.buzbuz.smartautoclicker.testing
 
 @Target(AnnotationTarget.CLASS)
 annotation class OpenForTesting
