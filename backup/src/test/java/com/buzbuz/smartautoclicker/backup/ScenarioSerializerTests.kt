aae1605401e4cda3b3f9257fa99e116067302545
Nain57 <buzeau.kevin@gmail.com>
2022-05-18 21:02:01 +0200

    Add few tests to serialization

--- backup/src/test/java/com/buzbuz/smartautoclicker/backup/ScenarioSerializerTests.kt
+++ backup/src/test/java/com/buzbuz/smartautoclicker/backup/ScenarioSerializerTests.kt
@@ -16,5 +16,110 @@
  */
 package com.buzbuz.smartautoclicker.backup
 
+import android.graphics.Point
+import android.os.Build
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.buzbuz.smartautoclicker.database.room.CLICK_DATABASE_VERSION
+
+import com.buzbuz.smartautoclicker.database.room.entity.*
+
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToStream
+import org.junit.Assert.assertEquals
+
+import org.junit.Assert.assertNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+
+/** Test the [ScenarioSerializer] class. */
+@OptIn(ExperimentalSerializationApi::class)
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.Q])
 class ScenarioSerializerTests {
+
+    companion object {
+        val DEFAULT_SCREEN_SIZE = Point(800,600)
+
+        val DEFAULT_COMPLETE_SCENARIO = CompleteScenario(
+            scenario = ScenarioEntity(1, "Scenario", 600, 1),
+            events = listOf(
+                CompleteEventEntity(
+                    event = EventEntity(1, 1, "Event", 1, 0, null),
+                    conditions = listOf(
+                        ConditionEntity(1, 1, "Condition", "/toto/tutu", 1, 2, 3, 4, 5, 1, true)
+                    ),
+                    actions = listOf(
+                        CompleteActionEntity(
+                            action = ActionEntity(1, 1, 0, "Intent", ActionType.INTENT,
+                                isAdvanced = false, isBroadcast = false, intentAction = "org.action", flags = 0,
+                                componentName = "org.action/org.action.TOTO",
+                            ),
+                            intentExtras = listOf(
+                                IntentExtraEntity(1, 1, IntentExtraType.BOOLEAN, "ExtraKey", "true")
+                            )
+                        )
+                    )
+                )
+            ),
+            endConditions = listOf(
+                EndConditionEntity(1, 1, 1, 20)
+            )
+        )
+    }
+
+    @Test
+    fun serialization_invalidVersion() {
+        val backup = ScenarioBackup(
+            version = 0,
+            scenario = DEFAULT_COMPLETE_SCENARIO,
+            screenWidth = DEFAULT_SCREEN_SIZE.x,
+            screenHeight = DEFAULT_SCREEN_SIZE.y,
+        )
+
+        ScenarioSerializer().apply {
+            val outputStream = ByteArrayOutputStream()
+            Json.encodeToStream(backup, outputStream)
+
+            assertNull(deserialize(ByteArrayInputStream(outputStream.toByteArray())))
+        }
+    }
+
+    @Test
+    fun serialization_sameVersion() {
+        val backup = ScenarioBackup(
+            version = CLICK_DATABASE_VERSION,
+            scenario = DEFAULT_COMPLETE_SCENARIO,
+            screenWidth = DEFAULT_SCREEN_SIZE.x,
+            screenHeight = DEFAULT_SCREEN_SIZE.y,
+        )
+
+        ScenarioSerializer().apply {
+            val outputStream = ByteArrayOutputStream()
+            Json.encodeToStream(backup, outputStream)
+
+            assertEquals(backup, deserialize(ByteArrayInputStream(outputStream.toByteArray())))
+        }
+    }
+
+    @Test
+    fun serialization_otherValidVersion() {
+        val backup = ScenarioBackup(
+            version = 19,
+            scenario = DEFAULT_COMPLETE_SCENARIO,
+            screenWidth = DEFAULT_SCREEN_SIZE.x,
+            screenHeight = DEFAULT_SCREEN_SIZE.y,
+        )
+
+        ScenarioSerializer().apply {
+            val outputStream = ByteArrayOutputStream()
+            Json.encodeToStream(backup, outputStream)
+
+            assertEquals(backup, deserialize(ByteArrayInputStream(outputStream.toByteArray())))
+        }
+    }
 }
