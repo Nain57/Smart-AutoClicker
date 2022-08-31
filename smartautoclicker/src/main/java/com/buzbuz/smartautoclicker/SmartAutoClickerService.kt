a4f81f36fe7c2804c192285c823e23fa7b2c9d1e
@BenjaminWegener <wegeneredv@gmail.com>
2022-09-01 00:14:15 +0200

    Update SmartAutoClickerService.kt

--- smartautoclicker/src/main/java/com/buzbuz/smartautoclicker/SmartAutoClickerService.kt
+++ smartautoclicker/src/main/java/com/buzbuz/smartautoclicker/SmartAutoClickerService.kt
@@ -184,6 +184,9 @@ class SmartAutoClickerService : AccessibilityService() {
             .setContentIntent(PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE))
             .setSmallIcon(R.drawable.ic_notification)
             .setOngoing(true)
+            .setCategory(Notification.CATEGORY_SERVICE)
+            .setOngoing(true)
+            .setLocalOnly(true)
             .setPriority(2)
             .build()
     }
