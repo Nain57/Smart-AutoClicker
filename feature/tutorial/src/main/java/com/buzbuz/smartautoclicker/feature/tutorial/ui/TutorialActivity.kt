a671a3d8b4933e8a2fbbf3436eaa3504a5d3303e
Nain57 <buzbuz.software@gmail.com>
2023-07-19 14:09:16 +0200

    Remove TutorialIntro

--- feature/tutorial/src/main/java/com/buzbuz/smartautoclicker/feature/tutorial/ui/TutorialActivity.kt
+++ feature/tutorial/src/main/java/com/buzbuz/smartautoclicker/feature/tutorial/ui/TutorialActivity.kt
@@ -22,8 +22,6 @@ import android.os.Bundle
 
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
 
 import com.buzbuz.smartautoclicker.feature.tutorial.R
 
@@ -32,34 +30,18 @@ class TutorialActivity : AppCompatActivity() {
 
     companion object {
 
-        private const val EXTRA_STARTING_PAGE = "extra_starting_page"
-
-        fun getStartIntent(context: Context, startingPage: TutorialStartingPage): Intent =
+        fun getStartIntent(context: Context): Intent =
             Intent(context, TutorialActivity::class.java)
                 .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                .putExtra(EXTRA_STARTING_PAGE, startingPage.name)
-
-        private fun Intent.getStartingPage(): TutorialStartingPage? {
-            val startingPageString = getStringExtra(EXTRA_STARTING_PAGE) ?: return null
-
-            return try {
-                TutorialStartingPage.valueOf(startingPageString)
-            } catch (ex: IllegalArgumentException) {
-                null
-            }
-        }
     }
 
     private val viewModel: TutorialViewModel by viewModels()
-
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tutorial)
 
         setupActionBar()
-        setupNavGraph(intent.getStartingPage())
     }
 
     override fun onStart() {
@@ -76,31 +58,4 @@ class TutorialActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.topAppBar))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
-
-    private fun setupNavGraph(startingPage: TutorialStartingPage?) {
-        supportFragmentManager.findFragmentById(R.id.nav_host_fragment)?.let { fragment ->
-            val navHostFragment = (fragment as NavHostFragment)
-
-            navController = navHostFragment.navController
-            val navGraph = navController.navInflater.inflate(R.navigation.nav_graph)
-
-            when (startingPage) {
-                TutorialStartingPage.INTRO -> navGraph.setStartDestination(R.id.fragment_intro)
-                TutorialStartingPage.LIST -> navGraph.setStartDestination(R.id.fragment_tutorial_list)
-                null -> finish()
-            }
-
-            navController.graph = navGraph
-        } ?: finish()
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        if (!navController.navigateUp()) finish()
-        return true
-    }
-}
-
-enum class TutorialStartingPage {
-    INTRO,
-    LIST,
 }
