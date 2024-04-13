5b8d15283d4dd19a945023a1ad0e6711ccf812ad
Kevin Buzeau <buzbuz.software@gmail.com>
2024-04-13 14:02:49 +0200

    Add hilt dependencies to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.googleDaggerHiltAndroid) apply false
     alias(libs.plugins.googleKsp) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.jetbrainsKotlinSerialization) apply false
