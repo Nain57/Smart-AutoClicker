27217ff3d5115f1b1bae2f97d473ed2b6d8bfb03
Nain57 <buzbuz.software@gmail.com>
2024-09-22 11:35:41 +0200

    Remove infinite repeat from dumb actions tries

--- core/dumb/src/main/java/com/buzbuz/smartautoclicker/core/dumb/engine/DumbActionTry.kt
+++ core/dumb/src/main/java/com/buzbuz/smartautoclicker/core/dumb/engine/DumbActionTry.kt
@@ -18,6 +18,9 @@ package com.buzbuz.smartautoclicker.core.dumb.engine
 
 import com.buzbuz.smartautoclicker.core.base.identifier.Identifier
 import com.buzbuz.smartautoclicker.core.dumb.domain.model.DumbAction
+import com.buzbuz.smartautoclicker.core.dumb.domain.model.DumbAction.DumbClick
+import com.buzbuz.smartautoclicker.core.dumb.domain.model.DumbAction.DumbPause
+import com.buzbuz.smartautoclicker.core.dumb.domain.model.DumbAction.DumbSwipe
 import com.buzbuz.smartautoclicker.core.dumb.domain.model.DumbScenario
 
 internal fun DumbAction.toDumbScenarioTry(): DumbScenario {
@@ -31,7 +34,21 @@ internal fun DumbAction.toDumbScenarioTry(): DumbScenario {
         maxDurationMin = 1,
         isDurationInfinite = false,
         randomize = false,
-        dumbActions = listOf(copyWithNewScenarioId(scenarioId = scenarioId))
+        dumbActions = listOf(toFiniteDumbAction(scenarioId))
     )
 }
 
+private fun DumbAction.toFiniteDumbAction(scenarioId: Identifier): DumbAction =
+    when (this) {
+        is DumbClick -> copy(
+            scenarioId = scenarioId,
+            isRepeatInfinite = false,
+        )
+        is DumbSwipe -> copy(
+            scenarioId = scenarioId,
+            isRepeatInfinite = false,
+        )
+        is DumbPause -> copy(
+            scenarioId = scenarioId
+        )
+    }
