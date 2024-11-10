2352ea41595aeb96a4f8928b5742e40122f76f3f
Nain57 <buzbuz.software@gmail.com>
2024-11-10 14:59:18 +0100

    Add Notification action UI

--- feature/smart-config/src/main/java/com/buzbuz/smartautoclicker/feature/smart/config/ui/common/model/action/UiNotification.kt
+++ feature/smart-config/src/main/java/com/buzbuz/smartautoclicker/feature/smart/config/ui/common/model/action/UiNotification.kt
@@ -23,6 +23,15 @@ import com.buzbuz.smartautoclicker.feature.smart.config.R
 
 
 @DrawableRes
-internal fun getNotificationIconRes(): Int = R.drawable.ic_notification
+internal fun getNotificationIconRes(): Int = R.drawable.ic_action_notification
 
-internal fun Notification.getDescription(context: Context, inError: Boolean): String = ""
+internal fun Notification.getDescription(context: Context, inError: Boolean): String {
+    if (inError) return context.getString(R.string.item_error_action_invalid_generic)
+
+    return when (messageType) {
+        Notification.MessageType.TEXT ->
+            context.getString(R.string.item_notification_details_text, messageText)
+        Notification.MessageType.COUNTER_VALUE ->
+            context.getString(R.string.item_notification_details_counter, messageCounterName)
+    }
+}
