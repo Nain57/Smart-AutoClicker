ce2b8bfc3c34986456c815a70486b3bc8a5cbd5d
Nain57 <buzbuz.software@gmail.com>
2024-12-22 11:22:59 +0100

    Move to Android SDK 35
    Impact:
    * Gradle wrapper updated to 8.9
    * AGP updated to 8.7.3
    * Java updated to 21

--- build-logic/convention/src/main/kotlin/com/buzbuz/gradle/convention/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/buzbuz/gradle/convention/AndroidApplicationConventionPlugin.kt
@@ -43,8 +43,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             }
 
             compileOptions.apply {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
+                sourceCompatibility = JavaVersion.VERSION_21
+                targetCompatibility = JavaVersion.VERSION_21
             }
 
             buildTypes {
