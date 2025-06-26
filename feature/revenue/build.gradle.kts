9b6e02b8a264c0cd7a8d04d30a2615fa1ebddef2
Nain57 <buzbuz.software@gmail.com>
2025-06-26 12:00:27 +0200

    Cleanup build parameter plugin

--- feature/revenue/build.gradle.kts
+++ feature/revenue/build.gradle.kts
@@ -15,9 +15,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import com.buzbuz.gradle.core.model.KlickrBuildType
-import com.buzbuz.gradle.core.extensions.isBuildForVariant
 import com.buzbuz.gradle.core.extensions.playStore
+import com.buzbuz.gradle.parameters.buildConfigField
+import com.buzbuz.gradle.parameters.manifestPlaceholders
 
 plugins {
     alias(libs.plugins.buzbuz.androidLibrary)
@@ -39,59 +39,13 @@ android {
                 viewBinding = true
             }
 
-            /**
-             * The devices to use as test device for the User Messaging SDK in debug builds only.
-             * To get the ID of a device, launch the app and request the user consent. The device ID will be printed in
-             * the logcat; see https://developers.google.com/admob/android/privacy?hl=fr#testing
-             */
-            buildParameters["consentTestDevicesIds"].asStringArrayBuildConfigField(this)
+            buildConfigField(buildParameters.consentTestDevicesIds)
+            buildConfigField(buildParameters.consentTestGeography)
+            buildConfigField(buildParameters.adsUnitId)
+            buildConfigField(buildParameters.adsTestDevicesIds)
+            buildConfigField(buildParameters.billingPublicKey)
 
-            /**
-             * The geographical area to use for the User Messaging SDK in debug builds only.
-             * Must be one of the values defined in ConsentDebugSettings.DebugGeography:
-             *  - DEBUG_GEOGRAPHY_DISABLED: 0
-             *  - DEBUG_GEOGRAPHY_EEA:      1
-             *  - DEBUG_GEOGRAPHY_NOT_EEA:  2
-             */
-            buildParameters["consentTestGeography"].asIntBuildConfigField(this, default = 0)
-
-            /** The identifier for the application for the AdMob SDK */
-            buildParameters["adsApplicationId"].apply {
-                asManifestPlaceHolder(this@playStore)
-            }
-            /**
-             * The advertisement block id for the AdMob SDK.
-             * Should have the following syntax: ca-app-pub-XXXXXXXXXXXXXXXX~XXXXXXXXXX
-             *
-             * For debug builds, you can use one of the following Sample ad unit ID:
-             *  - Interstitial:         ca-app-pub-3940256099942544/1033173712
-             *  - Interstitial Video:   ca-app-pub-3940256099942544/8691691433
-             *
-             * Note: If you are using the emulator or a physical device defined in adsTestDevicesIds, you can use your
-             * production ad unit ID.
-             */
-            buildParameters["adsUnitId"].apply {
-                val defaultAdsUnitId =
-                    if (project.isBuildForVariant(buildType = KlickrBuildType.RELEASE)) null
-                    else "ca-app-pub-3940256099942544/8691691433"
-                asStringBuildConfigField(this@playStore, default = defaultAdsUnitId)
-            }
-
-            /**
-             * The devices to use as test device for the AdMob SDK in debug builds only.
-             * If you are using your production adsApplicationId, you need to define all your physical test devices here
-             * To get the ID of a device, launch the app and goes to the paywall in order to load an ad. The device ID
-             * will be printed in the logcat; see https://developers.google.com/admob/android/test-ads?hl=fr#add_your_test_device_programmatically
-             */
-            buildParameters["adsTestDevicesIds"].asStringArrayBuildConfigField(this)
-
-            /**
-             * The application billing public key.
-             *
-             * You currently get this from the Google Play developer console under the "Monetization Setup" category
-             * in the Licensing area.
-             */
-            buildParameters["billingPublicKey"].asStringBuildConfigField(this)
+            manifestPlaceholders(buildParameters.adsApplicationId)
         }
     }
 }
