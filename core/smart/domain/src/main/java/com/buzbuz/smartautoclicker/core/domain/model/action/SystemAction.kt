5d920e4b41c89d9d8bdc8236f48697aeba234d44
Nain57 <buzbuz.software@gmail.com>
2025-07-12 14:32:34 +0200

    Add system actions to data model

---
+++ core/smart/domain/src/main/java/com/buzbuz/smartautoclicker/core/domain/model/action/SystemAction.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2025 Kevin Buzeau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.buzbuz.smartautoclicker.core.domain.model.action
+
+import com.buzbuz.smartautoclicker.core.base.identifier.Identifier
+import com.buzbuz.smartautoclicker.core.database.entity.SystemActionType
+
+data class SystemAction(
+    override val id: Identifier,
+    override val eventId: Identifier,
+    override val name: String?,
+    override var priority: Int,
+    val type: Type,
+) : Action() {
+
+
+    /**
+     * Types of toggle of a [SystemAction].
+     * Keep the same names as the db ones.
+     */
+    enum class Type {
+        /** Execute the Android back action. */
+        BACK,
+        /** Return to home launcher. */
+        HOME,
+        /** Open recent apps screen. */
+        RECENT_APPS,
+        /** Open the notification tray. */
+        OPEN_NOTIFICATIONS;
+
+        fun toEntity(): SystemActionType = SystemActionType.valueOf(name)
+    }
+
+    override fun hashCodeNoIds(): Int =
+        name.hashCode() + type.hashCode()
+
+    override fun deepCopy(): SystemAction = copy(name = "" + name)
+}
---
+++ core/smart/domain/src/main/java/com/buzbuz/smartautoclicker/core/domain/model/action/mapper/ActionEntityMapper.kt
@@ -0,0 +1,155 @@
+/*
+ * Copyright (C) 2025 Kevin Buzeau
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.buzbuz.smartautoclicker.core.domain.model.action.mapper
+
+import com.buzbuz.smartautoclicker.core.database.entity.ActionEntity
+import com.buzbuz.smartautoclicker.core.database.entity.ActionType
+import com.buzbuz.smartautoclicker.core.database.entity.CounterOperationValueType
+import com.buzbuz.smartautoclicker.core.domain.model.CounterOperationValue
+import com.buzbuz.smartautoclicker.core.domain.model.action.Action
+import com.buzbuz.smartautoclicker.core.domain.model.action.ChangeCounter
+import com.buzbuz.smartautoclicker.core.domain.model.action.Click
+import com.buzbuz.smartautoclicker.core.domain.model.action.Intent
+import com.buzbuz.smartautoclicker.core.domain.model.action.Notification
+import com.buzbuz.smartautoclicker.core.domain.model.action.Pause
+import com.buzbuz.smartautoclicker.core.domain.model.action.Swipe
+import com.buzbuz.smartautoclicker.core.domain.model.action.SystemAction
+import com.buzbuz.smartautoclicker.core.domain.model.action.ToggleEvent
+
+
+internal fun Action.toEntity(): ActionEntity {
+    if (!isComplete()) throw IllegalStateException("Can't transform to entity, action is incomplete: $this")
+
+    return when (this) {
+        is Click -> toClickEntity()
+        is Swipe -> toSwipeEntity()
+        is Pause -> toPauseEntity()
+        is Intent -> toIntentEntity()
+        is ToggleEvent -> toToggleEventEntity()
+        is ChangeCounter -> toChangeCounterEntity()
+        is Notification -> toNotificationEntity()
+        is SystemAction -> toSystemActionEntity()
+    }
+}
+
+private fun Click.toClickEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.CLICK,
+        pressDuration = pressDuration,
+        clickPositionType = positionType.toEntity(),
+        x = position?.x,
+        y = position?.y,
+        clickOnConditionId = clickOnConditionId?.databaseId,
+        clickOffsetX = clickOffset?.x,
+        clickOffsetY = clickOffset?.y,
+    )
+
+private fun Swipe.toSwipeEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.SWIPE,
+        swipeDuration = swipeDuration,
+        fromX = from?.x,
+        fromY = from?.y,
+        toX = to?.x,
+        toY = to?.y,
+    )
+
+private fun Pause.toPauseEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.PAUSE,
+        pauseDuration = pauseDuration,
+    )
+
+private fun Intent.toIntentEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.INTENT,
+        isAdvanced = isAdvanced,
+        isBroadcast = isBroadcast,
+        intentAction = intentAction,
+        componentName = componentName?.flattenToString(),
+        flags = flags,
+    )
+
+private fun ToggleEvent.toToggleEventEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.TOGGLE_EVENT,
+        toggleAllType = toggleAllType?.toEntity(),
+        toggleAll = toggleAll,
+    )
+
+private fun ChangeCounter.toChangeCounterEntity(): ActionEntity {
+    val isNumberValue = operationValue is CounterOperationValue.Number
+
+    return ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.CHANGE_COUNTER,
+        counterName = counterName,
+        counterOperation = operation.toEntity(),
+        counterOperationValueType = if (isNumberValue) CounterOperationValueType.NUMBER else CounterOperationValueType.COUNTER,
+        counterOperationValue = if (isNumberValue) operationValue.value as Int else null,
+        counterOperationCounterName = if (isNumberValue) null else operationValue.value as String,
+    )
+}
+
+private fun Notification.toNotificationEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.NOTIFICATION,
+        notificationImportance = channelImportance,
+        notificationMessageType = messageType.toEntity(),
+        notificationMessageText = messageText,
+        notificationMessageCounterName = messageCounterName,
+    )
+
+private fun SystemAction.toSystemActionEntity(): ActionEntity =
+    ActionEntity(
+        id = id.databaseId,
+        eventId = eventId.databaseId,
+        priority = priority,
+        name = name!!,
+        type = ActionType.SYSTEM,
+        systemActionType = type.toEntity(),
+    )
+
+
+
