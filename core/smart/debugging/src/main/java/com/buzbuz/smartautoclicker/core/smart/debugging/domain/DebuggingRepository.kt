9b48f951578f10d4a2fdb46476d20604223bfdca
Nain57 <buzbuz.software@gmail.com>
2025-12-16 13:56:51 +0100

    Implements new debug recorder engine

---
+++ core/smart/debugging/src/main/java/com/buzbuz/smartautoclicker/core/smart/debugging/domain/DebuggingRepository.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2025 Kevin Buzeau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.buzbuz.smartautoclicker.core.smart.debugging.domain
+
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.live.DebugLiveImageEventOccurrence
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.report.DebugReportEventOccurrence
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.report.DebugReportOverview
+import kotlinx.coroutines.flow.Flow
+
+
+interface DebuggingRepository {
+
+    /** Tells if a detection session is currently live debugged, iow, displaying the debug overlay while detecting. */
+    val isLiveDebugging: Flow<Boolean>
+
+    /** The last event that has been matched, with all interpreted conditions results. */
+    val lastImageEventFulfilled: Flow<DebugLiveImageEventOccurrence?>
+
+
+    /** Tells if the debugging overlay should be enabled while detecting. */
+    fun isDebugViewEnabled(): Boolean
+
+    /** Tells if a debug report should be created while detecting. */
+    fun isDebugReportEnabled(): Boolean
+
+    /** Replace the existing debug configuration settings with the provided ones. */
+    fun setDebuggingConfig(debugView: Boolean, debugReport: Boolean)
+
+    /** Read the last detection session report overview, if any. */
+    suspend fun getLastReportOverview(): DebugReportOverview?
+
+    /** Read the last detection session events occurrences. List will be empty no rapport is available. */
+    suspend fun getLastReportEventsOccurrences(): List<DebugReportEventOccurrence>
+
+    /** Tells if a debug report is available. */
+    suspend fun isDebugReportAvailable(): Boolean
+}
---
+++ core/smart/debugging/src/main/java/com/buzbuz/smartautoclicker/core/smart/debugging/engine/DebugEngine.kt
@@ -0,0 +1,226 @@
+/*
+ * Copyright (C) 2025 Kevin Buzeau
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.buzbuz.smartautoclicker.core.smart.debugging.engine
+
+import android.graphics.Rect
+import com.buzbuz.smartautoclicker.core.base.di.Dispatcher
+import com.buzbuz.smartautoclicker.core.base.di.HiltCoroutineDispatchers.IO
+import com.buzbuz.smartautoclicker.core.domain.model.event.ImageEvent
+import com.buzbuz.smartautoclicker.core.domain.model.event.TriggerEvent
+import com.buzbuz.smartautoclicker.core.domain.model.scenario.Scenario
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.DebuggingListener
+import com.buzbuz.smartautoclicker.core.smart.debugging.data.DebugConfigurationLocalDataSource
+import com.buzbuz.smartautoclicker.core.smart.debugging.data.DebugReportLocalDataSource
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.live.DebugLiveImageConditionResult
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.live.DebugLiveImageEventOccurrence
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.report.DebugReportConditionResult
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.report.DebugReportEventOccurrence
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.report.DebugReportOverview
+import com.buzbuz.smartautoclicker.core.smart.debugging.domain.model.result.ProcessedConditionResult
+import com.buzbuz.smartautoclicker.core.smart.debugging.engine.recorder.ImageEventOccurrenceRecorder
+import com.buzbuz.smartautoclicker.core.smart.debugging.engine.recorder.DebugReportOverviewRecorder
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+import javax.inject.Inject
+import javax.inject.Singleton
+import kotlin.collections.toList
+import kotlin.time.Duration.Companion.milliseconds
+
+
+/** Engine for the debugging of a scenario processing. */
+@Singleton
+internal class DebugEngine @Inject constructor(
+    @Dispatcher(IO) ioDispatcher: CoroutineDispatcher,
+    private val debugConfigurationLocalDataSource: DebugConfigurationLocalDataSource,
+    private val debugReportLocalDataSource: DebugReportLocalDataSource,
+) : DebuggingListener {
+
+    private val coroutineScopeIo: CoroutineScope =
+        CoroutineScope(SupervisorJob() + ioDispatcher.limitedParallelism(1))
+
+    private val overviewRecorder: DebugReportOverviewRecorder = DebugReportOverviewRecorder()
+    private val imgEventOccurrenceRecorder: ImageEventOccurrenceRecorder = ImageEventOccurrenceRecorder()
+
+    private var isReportEnabled: Boolean = false
+
+    private val _isDebuggingSession: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    val isDebuggingSession: StateFlow<Boolean> = _isDebuggingSession
+
+    private val _lastImageEventFulfilled: MutableStateFlow<DebugLiveImageEventOccurrence?> = MutableStateFlow(null)
+    val lastImageEventFulfilled: StateFlow<DebugLiveImageEventOccurrence?> = _lastImageEventFulfilled
+
+
+    override fun onSessionStarted(scenario: Scenario) {
+        coroutineScopeIo.launch {
+            isReportEnabled = debugConfigurationLocalDataSource.isDebugReportEnabled()
+            _isDebuggingSession.value = true
+
+            if (isReportEnabled) {
+                overviewRecorder.onSessionStart(scenario)
+                debugReportLocalDataSource.startReportWrite()
+            }
+        }
+    }
+
+    // Processing started on current frame
+    override fun onImageEventsProcessingStarted() {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            overviewRecorder.onFrameProcessingStarted()
+        }
+    }
+
+    // Processing started for current Event
+    override fun onImageEventProcessingStarted() {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            imgEventOccurrenceRecorder.onImageEventProcessingStarted()
+        }
+    }
+
+    // Condition is processed
+    override fun onImageConditionProcessingStarted() {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            imgEventOccurrenceRecorder.onImageConditionProcessingStarted()
+        }
+    }
+
+    override fun onImageConditionProcessingCompleted(result: ProcessedConditionResult.Image) {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            imgEventOccurrenceRecorder.onImageConditionProcessingCompleted(result)
+        }
+    }
+
+    // Processing ended for current Event
+    override fun onImageEventFulfilled(event: ImageEvent, results: List<ProcessedConditionResult.Image>) {
+        coroutineScopeIo.launch {
+            _lastImageEventFulfilled.update {
+                DebugLiveImageEventOccurrence(
+                    event = event,
+                    imageConditionsResults = results.map { result ->
+                        DebugLiveImageConditionResult(
+                            conditionId = result.condition.id.databaseId,
+                            isFulfilled = result.isFulfilled,
+                            isDetected = result.haveBeenDetected,
+                            confidenceRate = result.confidenceRate,
+                            detectionArea = result.getDetectionArea(),
+                        )
+                    },
+                )
+            }
+
+            if (isReportEnabled) {
+                debugReportLocalDataSource.writeEventOccurrenceToReport(
+                    occurrence = DebugReportEventOccurrence.ImageEvent(
+                        eventId = event.id.databaseId,
+                        relativeTimestampMs = overviewRecorder.sessionDurationMs,
+                        conditionsResults = imgEventOccurrenceRecorder.imageConditionResults.toList(),
+                    )
+                )
+                imgEventOccurrenceRecorder.reset()
+            }
+        }
+    }
+
+    // Processing ended on current frame
+    override fun onImageEventsProcessingCompleted() {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            overviewRecorder.onFrameProcessingStopped()
+        }
+    }
+
+    override fun onImageEventsProcessingCancelled() {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            overviewRecorder.onFrameProcessingStopped()
+            imgEventOccurrenceRecorder.reset()
+        }
+    }
+
+    override fun onTriggerEventProcessingCompleted(event: TriggerEvent, results: List<ProcessedConditionResult.Trigger>) {
+        coroutineScopeIo.launch {
+            if (!isReportEnabled) return@launch
+
+            debugReportLocalDataSource.writeEventOccurrenceToReport(
+                occurrence = DebugReportEventOccurrence.TriggerEvent(
+                    eventId = event.id.databaseId,
+                    relativeTimestampMs = overviewRecorder.sessionDurationMs,
+                    conditionsResults = results.map { result ->
+                        DebugReportConditionResult.TriggerCondition(
+                            conditionId = result.condition.id.databaseId,
+                            isFulFilled = result.isFulfilled,
+                        )
+                    }
+                )
+            )
+        }
+    }
+
+    override fun onSessionEnded() {
+        coroutineScopeIo.launch {
+            if (isReportEnabled) {
+                debugReportLocalDataSource.stopReportWrite(
+                    overview = DebugReportOverview(
+                        scenarioId = overviewRecorder.scenarioId,
+                        duration = overviewRecorder.sessionDurationMs.milliseconds,
+                        frameCount = overviewRecorder.frameCount,
+                        averageFrameProcessingDuration = overviewRecorder.averageFrameProcessingDurationMs.milliseconds,
+                        imageEventFulfilledCount = overviewRecorder.imageEventFulfilledCount,
+                        triggerEventFulfilledCount = overviewRecorder.triggerEventFulfilledCount,
+                    )
+                )
+
+                overviewRecorder.reset()
+            }
+
+            _lastImageEventFulfilled.value = null
+            _isDebuggingSession.value = false
+            isReportEnabled = false
+        }
+    }
+
+    private fun ProcessedConditionResult.Image.getDetectionArea(): Rect? {
+        val pos = position ?: return null
+        val halfWidth = condition.area.width() / 2
+        val halfHeight = condition.area.height() / 2
+
+
+        return if (pos.x == 0 && pos.y == 0) Rect()
+        else Rect(
+            pos.x - halfWidth,
+            pos.y - halfHeight,
+            pos.x + halfWidth,
+            pos.y + halfHeight,
+        )
+    }
+}
