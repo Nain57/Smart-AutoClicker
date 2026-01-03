0ed83c1e3e442b9b5664bd8d91c244730346de5a
Nain57 <buzbuz.software@gmail.com>
2026-01-03 14:31:08 +0100

    Use new way of defining java compile version in gradle

--- build-logic/obfuscation/build.gradle.kts
+++ build-logic/obfuscation/build.gradle.kts
@@ -15,6 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -30,8 +31,8 @@ java {
     targetCompatibility = JavaVersion.VERSION_21
 }
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_21.toString()
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
