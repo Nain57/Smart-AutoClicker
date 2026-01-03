3899c24863a26306280c9d7162dba37917acd321
Nain57 <buzbuz.software@gmail.com>
2026-01-03 14:31:08 +0100

    Use new way of defining java compile version in gradle

--- build-logic/source-download/build.gradle.kts
+++ build-logic/source-download/build.gradle.kts
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -29,8 +30,8 @@ java {
     targetCompatibility = JavaVersion.VERSION_21
 }
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_21.toString()
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_21)
     }
 }
 
