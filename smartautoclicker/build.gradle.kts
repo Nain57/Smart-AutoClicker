abcb3a96c3f12a8c480c685480ee04daa7472760
Nain57 <buzbuz.software@gmail.com>
2026-08-07 10:08:52 +0200

    Update version to 4.0.0-beta08

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -57,8 +57,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 92
-        versionName = "4.0.0-beta07"
+        versionCode = 93
+        versionName = "4.0.0-beta08"
     }
 
     if (project.isBuildForVariant(KlickrFlavour.F_DROID, KlickrBuildType.DEBUG)) {
