d1dec7bd34ac2bd30ed804d1f26f8c2dc0332db7
Nain57 <buzbuz.software@gmail.com>
2026-09-08 14:07:02 +0200

    Update version to 4.0.4

--- smartautoclicker/build.gradle.kts
+++ smartautoclicker/build.gradle.kts
@@ -57,8 +57,8 @@ android {
     defaultConfig {
         applicationId = getExtraActualApplicationId()
 
-        versionCode = 98
-        versionName = "4.0.3"
+        versionCode = 99
+        versionName = "4.0.4"
     }
 
     if (project.isBuildForVariant(KlickrFlavour.F_DROID, KlickrBuildType.DEBUG)) {
